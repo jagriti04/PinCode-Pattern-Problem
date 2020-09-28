50e8f9f9a53c642c87108007b1e583d03f878f64
Jagriti <jagriti.agni@gmail.com>
2020-09-28 21:05:32 +0530

    [Jagriti]ADDED: check pin with space

--- src/com/bridgelabzs/pincode/PinCodePattern.java
+++ src/com/bridgelabzs/pincode/PinCodePattern.java
@@ -11,10 +11,12 @@ public class PinCodePattern {
 		
 		System.out.println("Enter the pin code:");
 		String content = sc.nextLine();
-
+		
+		String newContent = content.replaceAll("\\s+","");			// remove the space from the entered pin code
+		System.out.println("Check the PIN code- " + newContent);
 		String pattern = "^([0-9]{6})$";
 		
-		boolean isMatch = Pattern.matches(pattern, content);
+		boolean isMatch = Pattern.matches(pattern, newContent);
 		
 		if (isMatch) {
 			System.out.println("The entered code is valid");
