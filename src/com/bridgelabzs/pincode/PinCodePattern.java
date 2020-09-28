0b469e774d6477223d35a2ec598ef66c7cd27736
Jagriti <jagriti.agni@gmail.com>
2020-09-28 19:42:08 +0530

    [Jagriti]ADDED: pattern checker for 6 digit pin

--- src/com/bridgelabzs/pincode/PinCodePattern.java
+++ src/com/bridgelabzs/pincode/PinCodePattern.java
@@ -1,8 +1,25 @@
 package com.bridgelabzs.pincode;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class PinCodePattern {
 	
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Welcome to pin code checker system");
+		
+		System.out.println("Enter the pin code:");
+		String content = sc.nextLine();
+
+		String pattern = "[0-9]{6}";
+		
+		boolean isMatch = Pattern.matches(pattern, content);
+		
+		if (isMatch) {
+			System.out.println("The entered code is valid");
+		} else {
+			System.out.println("The entered code is invalid");
+		}
 	}
 }
